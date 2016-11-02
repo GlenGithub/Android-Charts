29847947f571ac48946734d7b98d30b4f77bbbc8
ZhouRR <zhourr@ohs-sys.com>
2016-11-02 13:56:29 +0800

    merge Holsky with develop

--- androidcharts/src/main/java/cn/limc/androidcharts/common/CrossLines.java
+++ androidcharts/src/main/java/cn/limc/androidcharts/common/CrossLines.java
@@ -35,143 +35,136 @@ import cn.limc.androidcharts.axis.Axis;
 import cn.limc.androidcharts.view.DataGridChart;
 import cn.limc.androidcharts.view.GridChart;
 
-/** 
+/**
  * <p>en</p>
  * <p>jp</p>
  * <p>cn</p>
  *
- * @author limc 
- * @version v1.0 2014/06/24 18:30:44 
- *  
+ * @author limc
+ * @version v1.0 2014/06/24 18:30:44
  */
 public class CrossLines implements ICrossLines {
     protected GridChart inChart;
-    
-	/**
-	 * <p>
-	 * Color of cross line inside grid when touched
-	 * </p>
-	 * <p>
-	 * タッチしたポイント表示用十字線の色
-	 * </p>
-	 * <p>
-	 * 十字交叉线颜色
-	 * </p>
-	 */
-	private int crossLinesColor = DEFAULT_CROSS_LINES_COLOR;
 
-	/**
-	 * <p>
-	 * Color of cross line degree text when touched
-	 * </p>
-	 * <p>
-	 * タッチしたポイント表示用十字線度数文字の色
-	 * </p>
-	 * <p>
-	 * 十字交叉线坐标轴字体颜色
-	 * </p>
-	 */
-	private int crossLinesFontColor = DEFAULT_CROSS_LINES_FONT_COLOR;
-	
-	/**
-	 * <p>
-	 * Should display the Y cross line if grid is touched?
-	 * </p>
-	 * <p>
-	 * タッチしたポイントがある場合、十字線の垂直線を表示するか?
-	 * </p>
-	 * <p>
-	 * 在控件被点击时，显示十字竖线线
-	 * </p>
-	 */
-	private boolean displayCrossXOnTouch = DEFAULT_DISPLAY_CROSS_X_ON_TOUCH;
+    /**
+     * <p>
+     * Color of cross line inside grid when touched
+     * </p>
+     * <p>
+     * タッチしたポイント表示用十字線の色
+     * </p>
+     * <p>
+     * 十字交叉线颜色
+     * </p>
+     */
+    private int crossLinesColor = DEFAULT_CROSS_LINES_COLOR;
+
+    /**
+     * <p>
+     * Color of cross line degree text when touched
+     * </p>
+     * <p>
+     * タッチしたポイント表示用十字線度数文字の色
+     * </p>
+     * <p>
+     * 十字交叉线坐标轴字体颜色
+     * </p>
+     */
+    private int crossLinesFontColor = DEFAULT_CROSS_LINES_FONT_COLOR;
+
+    /**
+     * <p>
+     * Should display the Y cross line if grid is touched?
+     * </p>
+     * <p>
+     * タッチしたポイントがある場合、十字線の垂直線を表示するか?
+     * </p>
+     * <p>
+     * 在控件被点击时，显示十字竖线线
+     * </p>
+     */
+    private boolean displayCrossXOnTouch = DEFAULT_DISPLAY_CROSS_X_ON_TOUCH;
 
     private boolean displayCrossXDegreeOnTouch = DEFAULT_DISPLAY_CROSS_X_DEGREE_ON_TOUCH;
 
 
-
     /**
-	 * <p>
-	 * Should display the Y cross line if grid is touched?
-	 * </p>
-	 * <p>
-	 * タッチしたポイントがある場合、十字線の水平線を表示するか?
-	 * </p>
-	 * <p>
-	 * 在控件被点击时，显示十字横线线
-	 * </p>
-	 */
-	private boolean displayCrossYOnTouch = DEFAULT_DISPLAY_CROSS_Y_ON_TOUCH;
+     * <p>
+     * Should display the Y cross line if grid is touched?
+     * </p>
+     * <p>
+     * タッチしたポイントがある場合、十字線の水平線を表示するか?
+     * </p>
+     * <p>
+     * 在控件被点击时，显示十字横线线
+     * </p>
+     */
+    private boolean displayCrossYOnTouch = DEFAULT_DISPLAY_CROSS_Y_ON_TOUCH;
 
     private boolean displayCrossYDegreeOnTouch = DEFAULT_DISPLAY_CROSS_Y_DEGREE_ON_TOUCH;
 
     protected int bindCrossLinesToStick = DEFAULT_BIND_CROSS_LINES_TO_STICK;
-	
-	
-    public CrossLines(GridChart inChart){
+
+    public CrossLines(GridChart inChart) {
         super();
         this.inChart = inChart;
     }
-	
-	/**
-	 * @return the displayCrossXOnTouch
-	 */
-	public boolean isDisplayCrossXOnTouch() {
-		return displayCrossXOnTouch;
-	}
 
-	/**
-	 * @param displayCrossXOnTouch
-	 *            the displayCrossXOnTouch to set
-	 */
-	public void setDisplayCrossXOnTouch(boolean displayCrossXOnTouch) {
-		this.displayCrossXOnTouch = displayCrossXOnTouch;
-	}
+    /**
+     * @return the displayCrossXOnTouch
+     */
+    public boolean isDisplayCrossXOnTouch() {
+        return displayCrossXOnTouch;
+    }
 
-	/**
-	 * @return the displayCrossYOnTouch
-	 */
-	public boolean isDisplayCrossYOnTouch() {
-		return displayCrossYOnTouch;
-	}
+    /**
+     * @param displayCrossXOnTouch the displayCrossXOnTouch to set
+     */
+    public void setDisplayCrossXOnTouch(boolean displayCrossXOnTouch) {
+        this.displayCrossXOnTouch = displayCrossXOnTouch;
+    }
 
-	/**
-	 * @param displayCrossYOnTouch
-	 *            the displayCrossYOnTouch to set
-	 */
-	public void setDisplayCrossYOnTouch(boolean displayCrossYOnTouch) {
-		this.displayCrossYOnTouch = displayCrossYOnTouch;
-	}
-	
-	/**
-	 * @return the crossLinesColor
-	 */
-	public int getCrossLinesColor() {
-		return crossLinesColor;
-	}
+    /**
+     * @return the displayCrossYOnTouch
+     */
+    public boolean isDisplayCrossYOnTouch() {
+        return displayCrossYOnTouch;
+    }
 
-	/**
-	 * @param crossLinesColor
-	 *            the crossLinesColor to set
-	 */
-	public void setCrossLinesColor(int crossLinesColor) {
-		this.crossLinesColor = crossLinesColor;
-	}
+    /**
+     * @param displayCrossYOnTouch the displayCrossYOnTouch to set
+     */
+    public void setDisplayCrossYOnTouch(boolean displayCrossYOnTouch) {
+        this.displayCrossYOnTouch = displayCrossYOnTouch;
+    }
 
-	/**
-	 * @return the crossLinesFontColor
-	 */
-	public int getCrossLinesFontColor() {
-		return crossLinesFontColor;
-	}
+    /**
+     * @return the crossLinesColor
+     */
+    public int getCrossLinesColor() {
+        return crossLinesColor;
+    }
 
-	/**
-	 * @param crossLinesFontColor
-	 *            the crossLinesFontColor to set
-	 */
-	public void setCrossLinesFontColor(int crossLinesFontColor) {
-		this.crossLinesFontColor = crossLinesFontColor;
-	}
+    /**
+     * @param crossLinesColor the crossLinesColor to set
+     */
+    public void setCrossLinesColor(int crossLinesColor) {
+        this.crossLinesColor = crossLinesColor;
+    }
+
+    /**
+     * @return the crossLinesFontColor
+     */
+    public int getCrossLinesFontColor() {
+        return crossLinesFontColor;
+    }
+
+    /**
+     * @param crossLinesFontColor the crossLinesFontColor to set
+     */
+    public void setCrossLinesFontColor(int crossLinesFontColor) {
+        this.crossLinesFontColor = crossLinesFontColor;
+    }
 
     /**
      * @return the bindCrossLinesToStick
@@ -213,12 +206,12 @@ public class CrossLines implements ICrossLines {
      * <p>
      * 在图表被点击后绘制十字线
      * </p>
-     * 
+     *
      * @param canvas
      */
     protected void drawVerticalLine(Canvas canvas) {
 
-        
+
         PointF touchPoint = inChart.getTouchPoint();
         if (!displayCrossXOnTouch) {
             return;
@@ -237,7 +230,7 @@ public class CrossLines implements ICrossLines {
         mPaint.setPathEffect(new DashPathEffect(
                 new float[]{6, 3, 6, 3}, 1));
 
-        float lineVLength = inChart.getDataQuadrant().getHeight() 
+        float lineVLength = inChart.getDataQuadrant().getHeight()
                 + inChart.getAxisX().getLineWidth();
 
 
@@ -275,9 +268,9 @@ public class CrossLines implements ICrossLines {
 
     protected void drawHorizontalLine(Canvas canvas) {
 
-        
+
         PointF touchPoint = inChart.getTouchPoint();
-        
+
         if (!displayCrossYOnTouch) {
             return;
         }
@@ -293,7 +286,7 @@ public class CrossLines implements ICrossLines {
         mPaint.setStyle(Style.STROKE);
         mPaint.setStrokeWidth(2.0f);
         mPaint.setPathEffect(new DashPathEffect(
-                new float[] { 6, 3, 6, 3 }, 1));
+                new float[]{6, 3, 6, 3}, 1));
 
         float lineHLength = inChart.getDataQuadrant().getWidth() + inChart.getAxisY().getLineWidth();
 
@@ -305,7 +298,7 @@ public class CrossLines implements ICrossLines {
 
         if (displayCrossYDegreeOnTouch) {
             int fontSize = inChart.getSimpleGrid().getLatitudeFontSize();
-            String textToDraw = inChart.calcAxisYGraduate();
+            String textToDraw = inChart.calcAxisYGraduate();;
 
             Paint mPaintBox = new Paint();
             mPaintBox.setColor(Color.LTGRAY);
@@ -328,9 +321,9 @@ public class CrossLines implements ICrossLines {
             canvas.drawText(textToDraw, boxHS.x, boxHS.y + fontSize, mPaintBoxText);
         }
     }
-    
-    public void draw(Canvas canvas){
-        if (displayCrossXOnTouch|| displayCrossYOnTouch) {
+
+    public void draw(Canvas canvas) {
+        if (displayCrossXOnTouch || displayCrossYOnTouch) {
             drawHorizontalLine(canvas);
             drawVerticalLine(canvas);
         }
