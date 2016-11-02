d1fb05368313efc3fae1cbd52a88c24487b5a94d
ZhouRR <zhourr@ohs-sys.com>
2016-11-02 14:04:40 +0800

    merge Holsky with develop

--- androidcharts/src/main/java/cn/limc/androidcharts/common/ICrossLines.java
+++ androidcharts/src/main/java/cn/limc/androidcharts/common/ICrossLines.java
@@ -24,60 +24,58 @@ package cn.limc.androidcharts.common;
 
 import android.graphics.Color;
 
-/** 
+/**
  * <p>en</p>
  * <p>jp</p>
  * <p>cn</p>
  *
- * @author limc 
- * @version v1.0 2014/06/18 14:46:30 
- *  
+ * @author limc
+ * @version v1.0 2014/06/18 14:46:30
  */
 public interface ICrossLines {
-	static final int BIND_TO_TYPE_NONE = 0;
-	static final int BIND_TO_TYPE_HIRIZIONAL = 1;
-	static final int BIND_TO_TYPE_VERTICAL = 2;
-	static final int BIND_TO_TYPE_BOTH = 3;
-	
-	static final int DISPLAY_NONE = 0;
-	static final int DISPLAY_HIRIZIONAL = 1;
-	static final int DISPLAY_VERTICAL = 2;
-	static final int DISPLAY_BOTH = 3;
-	
-	static final int DEFAULT_CROSS_LINES_COLOR = Color.CYAN;
-	static final int DEFAULT_CROSS_LINES_FONT_COLOR = Color.CYAN;
-	
-	/**
-	 * <p>
-	 * Should display the Y cross line if grid is touched?
-	 * </p>
-	 * <p>
-	 * タッチしたポイントがある場合、十字線の垂直線を表示するか?
-	 * </p>
-	 * <p>
-	 * 默认在控件被点击时，显示十字竖线线
-	 * </p>
-	 */
-	public static final boolean DEFAULT_DISPLAY_CROSS_X_ON_TOUCH = false;
-	public static final boolean DEFAULT_DISPLAY_CROSS_X_DEGREE_ON_TOUCH = true;
+    static final int BIND_TO_TYPE_NONE = 0;
+    static final int BIND_TO_TYPE_HIRIZIONAL = 1;
+    static final int BIND_TO_TYPE_VERTICAL = 2;
+    static final int BIND_TO_TYPE_BOTH = 3;
+
+    static final int DISPLAY_NONE = 0;
+    static final int DISPLAY_HIRIZIONAL = 1;
+    static final int DISPLAY_VERTICAL = 2;
+    static final int DISPLAY_BOTH = 3;
+
+    static final int DEFAULT_CROSS_LINES_COLOR = Color.CYAN;
+    static final int DEFAULT_CROSS_LINES_FONT_COLOR = Color.CYAN;
+
+    /**
+     * <p>
+     * Should display the Y cross line if grid is touched?
+     * </p>
+     * <p>
+     * タッチしたポイントがある場合、十字線の垂直線を表示するか?
+     * </p>
+     * <p>
+     * 默认在控件被点击时，显示十字竖线线
+     * </p>
+     */
+    public static final boolean DEFAULT_DISPLAY_CROSS_X_ON_TOUCH = false;
+    public static final boolean DEFAULT_DISPLAY_CROSS_X_DEGREE_ON_TOUCH = true;
 
 
-	/**
-	 * <p>
-	 * Should display the Y cross line if grid is touched?
-	 * </p>
-	 * <p>
-	 * タッチしたポイントがある場合、十字線の水平線を表示するか?
-	 * </p>
-	 * <p>
-	 * 默认在控件被点击时，显示十字横线线
-	 * </p>
-	 */
-	public static final boolean DEFAULT_DISPLAY_CROSS_Y_ON_TOUCH = false;
-	public static final boolean DEFAULT_DISPLAY_CROSS_Y_DEGREE_ON_TOUCH = true;
+    /**
+     * <p>
+     * Should display the Y cross line if grid is touched?
+     * </p>
+     * <p>
+     * タッチしたポイントがある場合、十字線の水平線を表示するか?
+     * </p>
+     * <p>
+     * 默认在控件被点击时，显示十字横线线
+     * </p>
+     */
+    public static final boolean DEFAULT_DISPLAY_CROSS_Y_ON_TOUCH = false;
+    public static final boolean DEFAULT_DISPLAY_CROSS_Y_DEGREE_ON_TOUCH = true;
 
 
+    public static final int DEFAULT_BIND_CROSS_LINES_TO_STICK = BIND_TO_TYPE_HIRIZIONAL;
 
-	public static final int DEFAULT_BIND_CROSS_LINES_TO_STICK = BIND_TO_TYPE_HIRIZIONAL;
-	
 }
